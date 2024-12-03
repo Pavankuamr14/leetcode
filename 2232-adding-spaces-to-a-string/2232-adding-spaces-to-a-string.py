3581498406fc967a6aab9afc76e4a9b2257065ef
K. PAVAN SAI KUMAR <100195964+Pavankuamr14@users.noreply.github.com>
2024-12-03 15:34:28 +0530

    Time: 51 ms (81.05%), Space: 49.1 MB (35.18%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.py
@@ -6,13 +6,16 @@ class Solution:
             result+=s[i:size]
             result+=' '
             i=size
-            # print(i)
         result+=s[i:]
         return result
+        # Tc- O(n)
+        # space-O(n)
 
         # for i in range(len(s)):
         #     if i in spaces:
         #         result+=' '
         #     result+=s[i]
         # return result
+        # O(n*2)-TC
+        # O(n)space
         
