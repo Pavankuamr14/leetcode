6c34f0f2810287d1d501d03507ee230399d24002
K. PAVAN SAI KUMAR <100195964+Pavankuamr14@users.noreply.github.com>
2025-01-07 12:55:27 +0530

    Time: 6 ms (14.27%), Space: 17.8 MB (12.59%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,8 +1,22 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        s = ' '.join(words)
-        res = []
-        for w in words:
-            if s.count(w) > 1:
-                res.append(w)
-        return res
+        n = len(words)
+        ans = []
+
+        for i in range(n):
+            for j in range(n):
+                if i != j and words[j].find(words[i]) != -1:
+                    ans.append(words[i])
+                    break
+
+        return ans
+        # s = ' '.join(words)
+        # res = []
+        # for w in words:
+        #     if s.count(w) > 1:
+        #         res.append(w)
+        # return res
+        # edge case that you will have that are 
+        # if words list is like words=["leetcode", "solution", "des"] then ?
+        # if words list is like words=["abc", "zzza", "bczzz"] then ? we will fail to met the condition.
+        
