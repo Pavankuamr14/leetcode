f18d0c2aac287e4f5b92f01c0343b59b58728700
K. PAVAN SAI KUMAR <100195964+Pavankuamr14@users.noreply.github.com>
2025-01-10 20:34:17 +0530

    Time: 80 ms (99.2%), Space: 21.5 MB (5.58%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.py
+++ 0952-word-subsets/0952-word-subsets.py
@@ -1,21 +1,15 @@
-from collections import Counter
-from typing import List
-
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        # Calculate the maximum frequency of each character in all words2
-        max_freq = Counter()
-        for word in words2:
-            word_count = Counter(word)
-            for char in word_count:
-                max_freq[char] = max(max_freq[char], word_count[char])
-                print(max_freq)
-        
-        # Check for universal words in words1
-        result = []
-        for word in words1:
-            word_count = Counter(word)
-            if all(word_count[char] >= max_freq[char] for char in max_freq):
-                result.append(word)
-        
-        return result
+        ans = set(words1)
+        letters = {}
+        for i in words2:
+            for j in i:
+                count = i.count(j)
+                if j not in letters or count > letters[j]:
+                    letters[j] = count
+        for i in words1:
+            for j in letters:
+                if i.count(j) < letters[j]:
+                    ans.remove(i)
+                    break
+        return list(ans)
