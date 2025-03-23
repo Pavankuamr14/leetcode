3e75259c991575c56fc603ff77d544a0b3c675c5
K. PAVAN SAI KUMAR <100195964+Pavankuamr14@users.noreply.github.com>
2025-03-23 07:51:55 +0530

    Time: 1661 ms (46.51%), Space: 56.4 MB (55.52%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,7 +2,8 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         count=0
         marklist=[1]*(n+1)
-        for i in range(2,n+1):
+        # for i in range(2,n+1):
+        for i in range(2,int(n**0.5)+1):
             if marklist[i]==1:
                 for j in range(i*i,n,i):
                     marklist[j]=0
